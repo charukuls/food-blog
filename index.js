796b4f48b6a92a48b3005e27eef2bfcd1599b7ce
Charu Kulshrestha <charukuls@gmail.com>
2019-10-19 03:49:31 +0530

    add edge template

--- index.js
+++ index.js
@@ -2,13 +2,37 @@ const path = require('path');
 
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = new express();
+
+
+mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
+
+const { config, engine } = require('express-edge');
+
 
 app.use(express.static('public'));
 
+app.use(engine);
+app.set('views', `${__dirname}/views`);
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index');
 });
+
+
+
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+// });
 
 app.get('/about', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/about.html'));
